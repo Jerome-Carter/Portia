b2cfe02e0c44c06315d44fb19b400d2f9357e76d
Stone McLaren <stonemclaren13@gmail.com>
2022-03-25 21:38:28 -0400

    change up UI (#5)
    * added bootstrap to build
    
    * Basic UI setup
    
    Making basic UI with fake data getting it ready for the API
    
    Co-authored-by: Jerome Carter <Jerome-Carter@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, ListGroup, Badge,Nav } from 'react-bootstrap/'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">Portfolios</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">All Assets</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">News</Nav.Link>
+  </Nav.Item>
+</Nav>
+      <ListGroup as="ul" >
+  <ListGroup.Item
+    as="li"
+    className="d-flex justify-content-between align-items-start"
+  >
+    <div className="ms-2 me-auto">
+      <div className="fw-bold">Chase Bank</div>
+      
     </div>
+    <Badge bg="success" pill>
+      $1,400
+    </Badge>
+  </ListGroup.Item>
+  <ListGroup.Item
+    as="li"
+    className="d-flex justify-content-between align-items-start"
+  >
+    <div className="ms-2 me-auto">
+      <div className="fw-bold">Robinhood</div>
+      
+    </div>
+    <Badge bg="success" pill>
+      $500
+    </Badge>
+  </ListGroup.Item>
+  <ListGroup.Item
+    as="li"
+    className="d-flex justify-content-between align-items-start"
+  >
+    <div className="ms-2 me-auto">
+      <div className="fw-bold">Coinbase</div>
+      
+    </div>
+    <Badge bg="success" pill>
+      $200
+    </Badge>
+  </ListGroup.Item>
+</ListGroup>
+    <div>
+      <h3><strong>Total Value:</strong> $2,100</h3>
+    </div>
+    </div>
+
+    
   );
 }
 

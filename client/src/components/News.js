15b9f2aec9d7f0e8198f606439a07255cfc6426c
Stone McLaren <stonemclaren13@gmail.com>
2022-04-15 14:58:18 -0400

    Fixed rendering issues

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,62 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ListGroup, Button, Card } from 'react-bootstrap/';
 import './Portfolios.css';
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-
-let info = {content: [], url: []};
-let content =[]
 async function scrape() {
-axios.get("/r/wallstreetbets/search/?q=flair%3ADD&include_over_18=on&sort=new").then((res) => {
+    let temp = { content: [], url: [] };
+    const res = await axios.get("/r/wallstreetbets/search/?q=flair%3ADD&include_over_18=on&sort=new");
     //res.header("Access-Control-Allow-Origin", "*");
-    const $ =  cheerio.load(res.data);
-    
-    
-    $(`._eYtD2XCVieq6emjKBH3m`).each(function(i, elm) {
+    const $ = cheerio.load(res.data);
+
+
+    $(`._eYtD2XCVieq6emjKBH3m`).each(function (i, elm) {
         //console.log(i);
-        console.log($(elm).text()); // for testing do text() 
-        info.content.push($(elm).text());
-        
+        //console.log($(elm).text()); // for testing do text() 
+        temp.content.push($(elm).text());
+
     });
-    
-    $(".SQnoC3ObvgnGjWt90zD9Z._2INHSNB8V5eaWp4P0rY_mE").each(function(i,elm) {
+
+    $(".SQnoC3ObvgnGjWt90zD9Z._2INHSNB8V5eaWp4P0rY_mE").each(function (i, elm) {
         //console.log(i);
         //console.log($(elm).attr('href'));
-        info.url.push($(elm).attr('href'));
+        temp.url.push($(elm).attr('href'));
     });
+    let infoObj = []
+    temp.content.forEach((item, index) => {
+        //console.log(index);
+        infoObj.push({ content: item, url: "https://reddit.com" + temp.url[index] })
+    })
+    console.log(infoObj);
 
-    
-});
+    return infoObj
 }
-scrape();
-let infoList = []
-console.log(JSON.stringify(info))
-console.log(JSON.stringify(info))
-for (let i = 0; i < info.url.length; i++){
-    console.log(i);
-    infoList.push( <li key={i}>{info.content[i]}</li>)
-}
-console.log(content)
-content.forEach( (item, index) =>{
-    console.log(index);
-    infoList.push( <li key={index}>{item}</li> )
-})
-
-console.log(infoList);
 
 function News() {
-    
+
+    const [info, setInfo] = useState([]);
+
+    useEffect(() => {
+        scrape().then((data) => {
+            console.log(data);
+            setInfo(data);
+        })
+    }, []);
     return (
-        <>
-            <p>hi</p>
-            <ul>
-                {infoList}
-            </ul>
-        </>
-    );
+        
+        <ListGroup as="ul" >
+            <>
+            {info.map(data =>   
+        <ListGroup.Item
+                        as="li"
+                        className="d-flex justify-content-between align-items-start"
+                    >
+
+                     
+        <Card style={{ width: '18rem' }}>
+        <Card.Body>
+        <Card.Text>
+                {data.content}
+        </Card.Text>
+        <a href= {data.url} target="_blank"><Button variant="primary">Get More Knowledge</Button></a>
+        </Card.Body>
+        </Card>
+        </ListGroup.Item>
+        )}
+</>
+    </ListGroup>
     
+    );
+
 }
 export default News;
 
@@ -80,8 +93,16 @@ function News() {
         </ListGroup.Item>
 
     </ListGroup>
-            );
+            
         }
 
 export default News;
+
+
+<ul>
+                {info.map(data =>
+                    <li key={data.url}><a href={data.url}>{data.content}</a></li>
+
+                )}
+            </ul>
 */
